3af149b333c615a1af26683b51ab84621bfb8532
Harry Metske <harry.metske@gmail.com>
2012-12-09 19:51:39 +0100

    more tweaks for ACRA

--- src/nl/computerhok/send2mpd/Send2MPDApplication.java
+++ src/nl/computerhok/send2mpd/Send2MPDApplication.java
@@ -1,5 +1,6 @@
 package nl.computerhok.send2mpd;
 
+import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 
@@ -7,4 +8,11 @@ import android.app.Application;
 
 @ReportsCrashes(formKey = "", mailTo = "harry.metske@gmail.com", mode = ReportingInteractionMode.TOAST, resToastText = R.string.crash_toast_text)
 public class Send2MPDApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
 }
