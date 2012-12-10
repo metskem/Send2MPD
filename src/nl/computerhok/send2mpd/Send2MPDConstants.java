e843a9d9366f4b90a5622af3df8143341851384d
Harry Metske <harry.metske@gmail.com>
2012-12-10 21:39:09 +0100

    support move to SD (android:installLocation=auto)

--- src/nl/computerhok/send2mpd/Send2MPDConstants.java
+++ src/nl/computerhok/send2mpd/Send2MPDConstants.java
@@ -3,5 +3,5 @@ package nl.computerhok.send2mpd;
 public class Send2MPDConstants {
 
     /** The Version of Send2MPD. */
-    public static final String VERSION = "2012-12-09 20:43";
+    public static final String VERSION = "2012-12-10 21:38";
 }
