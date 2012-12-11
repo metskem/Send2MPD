045e21b75b27358c920a23b2a421c44cdc278536
Harry Metske <harry.metske@gmail.com>
2012-12-11 21:29:52 +0100

    minor layout changes

--- src/nl/computerhok/send2mpd/Send2MPDConstants.java
+++ src/nl/computerhok/send2mpd/Send2MPDConstants.java
@@ -3,5 +3,5 @@ package nl.computerhok.send2mpd;
 public class Send2MPDConstants {
 
     /** The Version of Send2MPD. */
-    public static final String VERSION = "2012-12-10 21:38";
+    public static final String VERSION = "2012-12-11 21:20";
 }
