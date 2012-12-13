d0e32fad7c85e2c08aeb9d8f5996a1f53702b850
Harry Metske <harry.metske@gmail.com>
2012-12-13 22:08:17 +0100

    * changed logo's * non-editable fields are now dark grey

--- src/nl/computerhok/send2mpd/Send2MPDConstants.java
+++ src/nl/computerhok/send2mpd/Send2MPDConstants.java
@@ -3,5 +3,5 @@ package nl.computerhok.send2mpd;
 public class Send2MPDConstants {
 
     /** The Version of Send2MPD. */
-    public static final String VERSION = "2012-12-11 21:20";
+    public static final String VERSION = "2012-12-13 21:50";
 }
